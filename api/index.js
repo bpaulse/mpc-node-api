eb850f83a667abd18b130e11b137d59f81c9889d
Bevan Paulse <bevan@datanav.co.za>
2025-08-16 12:02:56 +0200

    change format of index file

--- api/index.js
+++ api/index.js
@@ -1,27 +1,3 @@
-import express from "express";
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Example route
-app.get("/", (req, res) => {
-  res.send("Hello Bevan, your Node.js API is running (---) 🚀");
-});
-
-// Example API route
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from your Node.js API!" });
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
-
-// export default app;
-
-// Export the app for testing purposes
-// export { app };
+export default function handler(req, res) {
+  res.status(200).send("Welcome to my Node.js API on Vercel 🚀");
+}
